e8777cabaef99b278e489feb1f11599b522acc5f
Agata Gizińska <gizinska.agata@gmail.com>
2022-02-23 14:39:26 +0100

    Structure changes: introduced class Player to handle variables concerning players; introduced class Team to handle variables concerning teams; introduced class Dice to handle variables concerning dice number and size; included some game mechanics as methods in classes. Fix: deleted play_again function to avoid recursion; moved some global variables requiring input from user to main function

--- multiplayer_dice_roller.py
+++ multiplayer_dice_roller.py
@@ -1,123 +1,161 @@
-# This program is a multiplayer (2 - 10 players) dice game. User provides teams' and
-# players' names. Players are set into two teams randomly, choose dice size and number,
-# roll dice and get results in a table. After 10 rounds the program sums up results
-# and prints the outcome.
+#!/usr/bin/env python3
+
+# This program is a multiplayer (2 - 10 players) dice game. User provides
+# teams' and players' names. Players are set into two teams randomly, choose
+# dice size and number, roll dice and get results in a table. After 10 rounds
+# the program sums up results and prints the outcome.
 
 import random
-import pandas as pd
-
-team_name_1 = ''
-team_name_2 = ''
-team_1 = []
-team_2 = []
+import pandas
 
 
+class Player:
+
+    def __init__(self, name):
+        self.player_name = name
+        self.team = None
+
+    def __repr__(self):
+        return self.player_name
+
+    def assign_team(self, team):
+        self.team = team
+
+    @staticmethod
+    def roll_dice(dice):
+        dice_sum = 0
+        for i in range(0, dice.number):
+            roll = random.randint(1, dice.size)
+            dice_sum += roll
+            if roll == 1:
+                print(f'You rolled a {roll}! Critical Fail!')
+            elif roll == dice.size:
+                print(f'You rolled a {roll}! Critical Success!')
+            else:
+                print(f'You rolled a {roll}')
+        print(f'You have rolled a total of {dice_sum}')
+        return dice_sum
+
+
+class Team:
+
+    def __init__(self, name):
+        self.team_name = name
+        self.team_members = []
+        self.score = 0
+
+    def __repr__(self):
+        return self.team_name
+
+    def get_members(self, players):
+        players_ = players
+        for player in players_:
+            player.assign_team(self)
+            self.team_members.append(player)
+
+
+class Dice:
+
+    def __init__(self, number, size):
+        self.number = number
+        self.size = size
+
+
+# function to slow down gameplay to user-friendly level
 def wait():
     input('Press enter to continue...')
 
 
-def play_again():
-    question = input('Do you want to play again? (yes / no): ')
-    if question == 'yes':
-        main()
-    elif question == 'no':
-        print('Thank you for playing!')
-        exit()
-    else:
-        print('Invalid input')
-        exit()
-
-
-def set_teams():
-    global team_1
-    global team_2
-    global team_name_1
-    global team_name_2
-    team_name_1 = input('Name the first team: ')
-    team_name_2 = input('Name the second team: ')
+def set_teams(team1, team2, players_):
+    # create team objects
+    team_1 = Team(team1)
+    team_2 = Team(team2)
+    # get players' names and check if number of players is within range
+    players = players_
     while True:
-        try:
-            players = input('Enter names: ').split()
-            if len(players) in range(2, 11):
-                random.shuffle(players)
-                break
-            if len(players) < 2:
-                print('Minimum number of players is 2. Please provide valid number')
-            if len(players) > 10:
-                print('Maximum number of players is 10. Please provide valid number')
-        except Exception as e:
-            print(e)
-    mid_id = len(players) // 2
-    team_1 = (players[:mid_id])
-    team_2 = (players[mid_id:])
-    print(f'{team_name_1} players: {team_1} \n{team_name_2} players: {team_2}')
-    return team_1, team_2, team_name_1, team_name_2
+        if len(players) in range(2, 11):
+            random.shuffle(players)
+            break
+        if len(players) < 2:
+            print('Minimum number of players is 2. Please provide a valid '
+                  'number')
+        if len(players) > 10:
+            print('Maximum number of players is 10. Please provide a valid '
+                  'number')
+    # create player objects for each prompted name
+    player_objects = []
+    for player_name in players:
+        player = Player(player_name)
+        player_objects.append(player)
+    # shuffle created players into teams
+    mid_id = len(player_objects) // 2
+    team_1.get_members(player_objects[:mid_id])
+    team_2.get_members(player_objects[mid_id:])
+
+    print(f'{team_1.team_name} players: '
+          f'{str(team_1.team_members).strip("[]")} \n'
+          f'{team_2.team_name} players: '
+          f'{str(team_2.team_members).strip("[]")}')
+
+    return team_1, team_2
 
 
-def game():
-    list_of_rounds = []
+def game(team1, team2):
+    # create list of rounds for output table rows
+    rounds = []
     for number in range(1, 11):
-        list_of_rounds.append(str(number))
+        rounds.append(str(number))
+    # set dice number and size
     dice_rolls = int(input('How many dice would you like to roll? '))
     dice_size = int(input('How many sides are the dice? '))
+    dice = Dice(dice_rolls, dice_size)
     wait()
-    df = pd.DataFrame(index=list_of_rounds, columns=[team_name_1, team_name_2], data=0)
-    for round_number in list_of_rounds:
+    # create Dataframe for output table
+    df = pandas.DataFrame(index=rounds, columns=[team1, team2], data=0)
+    # iterate over rounds
+    for round_number in rounds:
+        # show current scores and start next round
         print(df)
-        print(f'round number {round_number} - start'.upper())
-        result_team_1 = 0
-        result_team_2 = 0
-        for player in team_1:
+        print(f'Round number {round_number} - start'.upper())
+        # team 1 members roll dice
+        score_team1 = 0
+        for player in team1.team_members:
             print(f'Player {player} - roll dice!')
             wait()
-            dice_sum = 0
-            for i in range(0, dice_rolls):
-                roll = random.randint(1, dice_size)
-                dice_sum += roll
-                if roll == 1:
-                    print(f'You rolled a {roll}! Critical Fail!')
-                elif roll == dice_size:
-                    print(f'You rolled a {roll}! Critical Success!')
-                else:
-                    print(f'You rolled a {roll}')
-            print(f'You have rolled a total of {dice_sum}')
-            result_team_1 += dice_sum
-        print(f'The result for {team_name_1} in round {round_number} is {result_team_1}!')
-        for player in team_2:
+            score_team1 += player.roll_dice(dice)
+        print(f'The score for {team1.team_name} in round {round_number} is '
+              f'{score_team1}!')
+        # team 2 members roll dice
+        score_team2 = 0
+        for player in team2.team_members:
             print(f'Player {player} - roll dice!')
             wait()
-            dice_sum = 0
-            for i in range(0, dice_rolls):
-                roll = random.randint(1, dice_size)
-                dice_sum += roll
-                if roll == 1:
-                    print(f'You rolled a {roll}! Critical Fail!')
-                elif roll == dice_size:
-                    print(f'You rolled a {roll}! Critical Success!')
-                else:
-                    print(f'You rolled a {roll}')
-            print(f'You have rolled a total of {dice_sum}')
-            result_team_2 += dice_sum
-        print(f'The result for {team_name_2} in round {round_number} is {result_team_2}!')
-        df.at[round_number, team_name_1] = result_team_1
-        df.at[round_number, team_name_2] = result_team_2
+            score_team2 += player.roll_dice(dice)
+        print(f'The score for {team2.team_name} in round {round_number} is '
+              f'{score_team2}!')
+        # append results for the round to Dataframe table
+        df.at[round_number, team1] = score_team1
+        df.at[round_number, team2] = score_team2
+    # get final results for both teams
     results = list(df.sum(axis=0).values)
-    final_result_team_1 = results[0]
-    final_result_team_2 = results[1]
-    print(f'Final results:\n{team_name_1} - {final_result_team_1}\n'
-          f'{team_name_2} - {final_result_team_2}')
-    if final_result_team_1 > final_result_team_2:
-        print(f'{team_name_1} won!')
-    elif final_result_team_1 == final_result_team_2:
+    final_result_team1 = results[0]
+    final_result_team2 = results[1]
+    print(f'Final results:\n{team1} - {final_result_team1}\n'
+          f'{team2} - {final_result_team2}')
+    if final_result_team1 > final_result_team2:
+        print(f'{team1} won!')
+    elif final_result_team1 == final_result_team2:
         print('It\'s a tie!')
     else:
-        print(f'{team_name_2} won!')
-    play_again()
+        print(f'{team2} won!')
 
 
 def main():
-    set_teams()
-    game()
+    team_name_1 = input('Name the first team: ')
+    team_name_2 = input('Name the second team: ')
+    players = input('Enter space-separated players\' names: ').split()
+    teams = list(set_teams(team_name_1, team_name_2, players))
+    game(teams[0], teams[1])
 
 
 if __name__ == "__main__":
